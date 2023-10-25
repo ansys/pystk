098e92d29bf34bfc381efb84514e4369209faa80
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2023-10-25 17:59:34 +0200

    doc: update to incomming changes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -42,6 +42,7 @@ html_theme_options = {
         "version_match": get_version_match(__version__),
     },
     "check_switcher": False,
+    "navigation_with_keys": True,
 }
 html_static_path = ["_static"]
 html_css_files = [
