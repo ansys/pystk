a046c42d2acbc09b8fc80c7eafdfd940ebf86105
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2023-12-21 18:49:28 +0100

    fix: tox

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -46,6 +46,7 @@ html_theme_options = {
 }
 html_static_path = ["_static"]
 html_css_files = [
+    "_static/css",
     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
 ]
 
